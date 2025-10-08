46320968eb6c00fdcb442b38519df1bebedcd481
margaeery <margorazhina@mail.ru>
2025-10-08 21:04:48 +0300

    Add program for finding gcd

---
+++ 2/4.py
@@ -0,0 +1,11 @@
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return abs(a)
+
+try:
+    num1 = int(input("Введите первое число: "))
+    num2 = int(input("Введите второе число: "))
+    print(f"НОД({num1}, {num2}) = {gcd(num1, num2)}")
+except ValueError:
+    print("Ошибка! Пожалуйста, введите целые числа.")
