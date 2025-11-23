13644bad9ca15d4cf2bce65b26b9ffb3a07f2675
margaeery <margorazhina@mail.ru>
2025-11-23 11:15:33 +0300

    Add reduce

---
+++ 4/4.py
@@ -0,0 +1,10 @@
+from functools import reduce
+
+def factorial(n):
+    if n < 0:
+        return None
+    return reduce(lambda x, y: x * y, range(1, n + 1),1)
+
+print(factorial(-5))
+print(factorial(0))
+print(factorial(5))
