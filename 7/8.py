1a95cedfa75cf852cb49be3dafb9e7acdb465025
margaeery <margorazhina@mail.ru>
2025-12-15 21:31:56 +0300

    Add JOIN

--- 7/8.py
+++ 7/8.py
@@ -4,7 +4,8 @@ conn = sqlite3.connect('8.db')
 cursor = conn.cursor()
 
 cursor.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT)")
-cursor.execute("CREATE TABLE orders (id INTEGER PRIMARY KEY, user_id INTEGER, product TEXT)")
+cursor.execute("CREATE TABLE orders (id INTEGER PRIMARY KEY, user_id INTEGER,"
+               " product TEXT)")
 
 cursor.execute("INSERT INTO users VALUES (1, 'Иван')")
 cursor.execute("INSERT INTO users VALUES (2, 'Мария')")
@@ -14,8 +15,15 @@ cursor.execute("INSERT INTO orders VALUES (1, 1, 'Книга')")
 cursor.execute("INSERT INTO orders VALUES (2, 1, 'Ноутбук')")
 cursor.execute("INSERT INTO orders VALUES (3, 2, 'Телефон')")
 
+conn.commit()
 
-for row in cursor.execute("SELECT users.name, orders.product FROM users JOIN orders ON users.id = orders.user_id"):
+query = """
+SELECT users.name, orders.product
+FROM users
+JOIN orders ON users.id = orders.user_id
+"""
+
+for row in cursor.execute(query):
     print(row)
 
 conn.close()
